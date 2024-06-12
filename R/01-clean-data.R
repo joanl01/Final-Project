c96d97005cb4e2538dee0ce2aaf517d61dd1047e
joanl01 <a1825938@adelaide.edu.au>
2024-06-12 12:15:02 +0930

    Code written to clean the raw dataset given by Dr Karl Berator
    The code was written to recode the cell line, treatment and name variable so that it is consistent across the dataset. The cleaned dataset was then saved as a csv file, "2024-04-05-gene-data.csv" in the data folder

---
+++ R/01-clean-data.R
@@ -0,0 +1,34 @@
+# Load required packages
+pacman::p_load(tidyverse, harrypotter,readxl)
+
+# Read in dataset
+df <- read_excel(here::here("raw-data", "2024-04-04-gene-data.xlsx"))
+# Get a glance on dataset
+head(df)
+
+# Check variable cell line
+table(df$cell_line)
+# Recode for consistency
+df$cell_line <- fct_recode(df$cell_line,
+                           "Wild-type"="WILD-TYPE",
+                           "Cell-type 101" = "CELL-TYPE 101")
+
+# Check variable on treatment
+table(df$treatment)
+# Recode treatment for consistency
+df$treatment <- fct_recode(df$treatment,
+                           "Activating factor 42" = "activating factor 42",
+                           "Placebo" = "placebo")
+
+# Check variable on name/gene line
+table(df$name)
+# Recode name for consistency
+df$name <- fct_recode(df$name,
+                      "GL-cwN" = "Gl-Cwn",
+                      "GL-rjS"="Gl-Rjs",
+                      "GL-XIb" = "Gl-Xib",
+                      "GL-ZHw"="Gl-Zhw"
+)
+# Want to find if there are effect of treatment on the effect of growth factor on the gene expression
+# Save cleaned data into csv
+write_csv(df, "data/2024-04-05-gene-data.csv")
